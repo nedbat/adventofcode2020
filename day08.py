b0bf912747355fb1340da419cf12b855c5331456
Ned Batchelder <ned@nedbatchelder.com>
2020-12-08 07:22:33 -0500

    Day 8, part 2

--- day08.py
+++ day08.py
@@ -19,14 +19,15 @@ def read_program(fname):
     return program
 
 class Cpu:
-    def __init__(self, program_name):
-        self.program = read_program(program_name)
+    def __init__(self, program, tweaks=None):
+        self.program = program
+        self.tweaks = tweaks or {}
         self.ip = 0
         self.acc = 0
         self.ips_executed = set()
 
     def step(self):
-        op, num = self.program[self.ip]
+        op, num = self.tweaks.get(self.ip, self.program[self.ip])
         self.ips_executed.add(self.ip)
         if op == Op.acc:
             self.acc += num
@@ -44,11 +45,44 @@ class Cpu:
             self.step()
         return self.acc
 
+    def done(self):
+        return self.ip == len(self.program)
+
+    def part2(self):
+        while True:
+            if self.about_to_repeat():
+                return None
+            if self.done():
+                return self.acc
+            self.step()
+
+
 def test_part1():
-    cpu = Cpu("day08_test.txt")
+    cpu = Cpu(read_program("day08_test.txt"))
     assert cpu.part1() == 5
 
 if __name__ == "__main__":
-    cpu = Cpu("day08_input.txt")
+    cpu = Cpu(read_program("day08_input.txt"))
     acc = cpu.part1()
     print(f"Part 1: Immediately before any instruction is executed a second time, {acc} is in the accumulator.")
+
+def part2(fname):
+    program = read_program(fname)
+    for i, (op, num) in enumerate(program):
+        if op == Op.jmp:
+            tweaks = {i: (Op.nop, num)}
+        elif op == Op.nop:
+            tweaks = {i: (Op.jmp, num)}
+        elif op == Op.acc:
+            continue
+        cpu = Cpu(program, tweaks)
+        acc = cpu.part2()
+        if acc is not None:
+            return acc
+
+def test_part2():
+    assert part2("day08_test.txt") == 8
+
+if __name__ == "__main__":
+    acc = part2("day08_input.txt")
+    print(f"Part 2: {acc} is in the accumulator.")
