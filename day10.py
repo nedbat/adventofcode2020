acf12ca890d9106fdc8e78fb4658e86b2215daac
Ned Batchelder <ned@nedbatchelder.com>
2020-12-10 07:11:18 -0500

    Day 10, part 2

--- day10.py
+++ day10.py
@@ -31,3 +31,30 @@ def test_part1(fname, ans):
 if __name__ == '__main__':
     ans = part1("day10_input.txt")
     print(f"Part 1: the number of 1-jolt differences multiplied by the number of 3-jolt differences is {ans}")
+
+def arrangements(nums):
+    nums = sorted(nums, reverse=True) + [0]
+    ways = [1]
+    for i in range(1, len(nums)):
+        this_way = 0
+        for back in [1,2,3]:
+            backi = i - back
+            if backi >= 0:
+                if 1 <= nums[backi] - nums[i] <= 3:
+                    this_way += ways[backi]
+        ways.append(this_way)
+    return ways[-1]
+
+def part2(fname):
+    return arrangements(numbers(fname))
+
+@pytest.mark.parametrize("fname, ans", [
+    ("day10_test1.txt", 8),
+    ("day10_test2.txt", 19208),
+])
+def test_arrangements(fname, ans):
+    assert part2(fname) == ans
+
+if __name__ == '__main__':
+    ans = part2("day10_input.txt")
+    print(f"Part 2: the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device is {ans}")
