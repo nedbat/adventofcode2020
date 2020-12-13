10a2fa9ddd763756eb386209a29aad1c4bd2cfcc
Ned Batchelder <ned@nedbatchelder.com>
2020-12-13 09:09:15 -0500

    Day 13, part 2

--- day13.py
+++ day13.py
@@ -1,10 +1,16 @@
 # https://adventofcode.com/2020/day/13
 
+import functools
+
+import pytest
+
+from helpers import lcm, modular_inverse, product
+
 x = None
 
 TEST = (
     939,
-    {7,13,x,x,59,x,31,19}
+    [7,13,x,x,59,x,31,19]
 )
 
 def part1(time_now, bus_ids):
@@ -17,9 +23,47 @@ def test_part1():
 
 INPUT = (
     1001612,
-    {19,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,821,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,463,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23}
+    [19,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,821,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,463,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23]
 )
 
 if __name__ == '__main__':
     ans = part1(*INPUT)
     print(f"Part 1: {ans}")
+
+# Part 2:
+# For the TEST input, we need to find x such that:
+#   x % 7 == 0
+#   x % 13 == 13 - 1
+#   x % 59 == 59 - 4
+#   x % 31 == 31 - 6
+#   x % 19 == 19 - 7
+#
+#   (x * minv(x,m)) % m == 1
+# 
+# https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+
+def part2(bus_ids):
+    base_mods = [(b, m) for m, b in enumerate(bus_ids) if b is not None]
+    bases = [b for b, _ in base_mods]
+    p = product(bases)
+    x = 0
+    for b, m in base_mods:
+        pp = p // b
+        x += modular_inverse(pp, b) * pp * (b - m)
+    return x % lcm(bases)
+
+
+@pytest.mark.parametrize("bus_ids, ans", [
+    (TEST[1], 1068781),
+    ([17,x,13,19], 3417),
+    ([67,7,59,61], 754018),
+    ([67,x,7,59,61], 779210),
+    ([67,7,x,59,61], 1261476),
+    ([1789,37,47,1889], 1202161486),
+])
+def test_part2(bus_ids, ans):
+    assert part2(bus_ids) == ans
+
+if __name__ == '__main__':
+    ans = part2(INPUT[1])
+    print(f"Part 2: {ans}")
---
+++ helpers.py
@@ -0,0 +1,43 @@
+import functools
+import math
+
+def product(nums):
+    return functools.reduce((lambda a, b: a * b), nums)
+
+def lcm2(a, b):
+    return int(a * b / math.gcd(a, b))
+
+def lcm(nums):
+    return functools.reduce(lcm2, nums)
+
+def modular_inverse(a, m):
+    m0 = m
+    y = 0
+    x = 1
+
+    if m == 1:
+        return 0
+
+    while a > 1:
+        # q is quotient
+        if m == 0:
+            return None
+        q = a // m
+
+        t = m
+
+        # m is remainder now, process
+        # same as Euclid's algo
+        m = a % m
+        a = t
+        t = y
+
+        # Update x and y
+        y = x - q * y
+        x = t
+
+    # Make x positive
+    if x < 0:
+        x += m0
+
+    return x
