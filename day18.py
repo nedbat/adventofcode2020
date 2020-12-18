e1e75b06323440876448f006d3f2784a362c0233
Ned Batchelder <ned@nedbatchelder.com>
2020-12-18 07:52:57 -0500

    Day 18, part 2

--- day18.py
+++ day18.py
@@ -49,3 +49,66 @@ if __name__ == "__main__":
     with open("day18_input.txt") as f:
         ans = sum(eval(line) for line in f)
     print(f"Part 1: {ans}")
+
+
+# Part 2 needs a real parser
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = iter(tokens)
+        self.eat()
+
+    def eat(self):
+        self.token = next(self.tokens, "eof")
+
+    def expect(self, tok):
+        if self.token == tok:
+            self.eat()
+        else:
+            raise Exception(f"Expected {tok!r}, had {self.token!r}")
+
+    def star(self):
+        val = self.plus()
+        while self.token == "*":
+            self.eat()
+            val *= self.plus()
+        return val
+
+    def plus(self):
+        val = self.value()
+        while self.token == "+":
+            self.eat()
+            val += self.value()
+        return val
+
+    def value(self):
+        if self.token == "(":
+            self.eat()
+            val = self.star()
+            self.expect(")")
+        else:
+            val = int(self.token)
+            self.eat()
+        return val
+
+def eval2(expr):
+    toks = iter(re.findall(r"(\d+|\S)", expr))
+    val = Parser(toks).star()
+    return val
+
+@pytest.mark.parametrize("expr, ans", [
+    ("17", 17),
+    ("1 + 2 * 3 + 4 * 5 + 6", 231),
+    ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+    ("2 * 3 + (4 * 5)", 46),
+    ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+    ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+    ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340),
+])
+def test_eval2(expr, ans):
+    assert eval2(expr) == ans
+
+if __name__ == "__main__":
+    with open("day18_input.txt") as f:
+        ans = sum(eval2(line) for line in f)
+    print(f"Part 2: {ans}")
